3deb78e5b8a927ddfa88eaf60699f2ca2616363b
chanmyaeoo <chanmyaeoo1996mgk@gmail.com>
2020-02-11 11:17:24 +0630

    added game status

--- hangman/app.js
+++ hangman/app.js
@@ -5,12 +5,12 @@ const puzzleEl = document.querySelector('#puzzle');
 const guessCountEl = document.querySelector('#guessCount');
 
 puzzleEl.textContent = hangman1.getPuzzle();
-guessCountEl.textContent = hangman1.remainingGuesses;
+guessCountEl.textContent = hangman1.getStatusMessage();
 
 window.addEventListener('keypress', e => {
   const guess = String.fromCharCode(e.charCode);
 
   hangman1.makeGuess(guess);
   puzzleEl.textContent = hangman1.getPuzzle();
-  guessCountEl.textContent = hangman1.remainingGuesses;
+  guessCountEl.textContent = hangman1.getStatusMessage();
 });
--- hangman/hangman.js
+++ hangman/hangman.js
@@ -2,6 +2,45 @@ const Hangman = function(word, remainingGuesses) {
   this.word = word.toLowerCase().split('');
   this.remainingGuesses = remainingGuesses;
   this.guessLetter = [];
+  this.status = 'playing';
+};
+
+Hangman.prototype.getStatusMessage = function() {
+  if (this.status === 'playing') {
+    return `Guess left: ${this.remainingGuesses}`;
+  } else if (this.status === 'failed') {
+    return `Nice try. The word was "${this.word.join('')}".`;
+  } else {
+    return 'Great work. You guessed the word.';
+  }
+};
+
+Hangman.prototype.calculateStatus = function() {
+  // Problem solving 3
+  const finished = this.word.every(letter => this.guessLetter.includes(letter));
+  // Problem solving 2
+  // const guessLetter = this.word.filter(letter => {
+  //   return !this.guessLetter.includes(letter);
+  // });
+
+  // let finished = guessLetter.length === 0;
+
+  // Problem solving 1
+  // let finished = true;
+  // this.word.forEach(letter => {
+  //   if (this.guessLetter.includes(letter)) {
+  //     finished = true;
+  //   } else {
+  //     finished = false;
+  //   }
+  // });
+  if (this.remainingGuesses === 0) {
+    this.status = 'failed';
+  } else if (finished) {
+    this.status = 'finished';
+  } else {
+    this.status = 'playing';
+  }
 };
 
 Hangman.prototype.getPuzzle = function() {
@@ -20,10 +59,16 @@ Hangman.prototype.makeGuess = function(guess) {
   guess = guess.toLowerCase();
   const isUnique = !this.guessLetter.includes(guess);
   const isBadGuess = !this.word.includes(guess);
+
+  if (this.status !== 'playing') {
+    return;
+  }
   if (isUnique) {
     this.guessLetter.push(guess);
   }
   if (isUnique && isBadGuess) {
     this.remainingGuesses--;
   }
+
+  this.calculateStatus();
 };
