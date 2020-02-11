50fe1b6719fb59b526bc4799d7b5876d06f9d72f
chanmyaeoo <chanmyaeoo1996mgk@gmail.com>
2020-02-11 11:39:45 +0630

    changed class syntax

--- oop/constructor-function.js
+++ oop/constructor-function.js
@@ -1,23 +1,26 @@
-const Person = function(firstName, lastName, age, likes = []) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.age = age;
-  this.likes = likes;
-};
+class Person {
+  constructor(firstName, lastName, age, likes = []) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.likes = likes;
+  }
 
-Person.prototype.getBio = function() {
-  let bio = `${this.firstName} is ${this.age}`;
-  this.likes.forEach(like => {
-    bio += ` ${this.firstName} likes ${like}`;
-  });
-  return bio;
-};
+  getBio() {
+    let bio = `${this.firstName} is ${this.age}`;
+    this.likes.forEach(like => {
+      bio += ` ${this.firstName} likes ${like}`;
+    });
+    return bio;
+  }
 
-Person.prototype.setName = function(fullName) {
-  let name = fullName.split(' ');
-  this.firstName = name[0];
-  this.lastName = name[1];
-};
+  setName(fullName) {
+    let name = fullName.split(' ');
+    this.firstName = name[0];
+    this.lastName = name[1];
+  }
+}
+
 const person1 = new Person('Jen', 'Ledge', 27, ['travelling', 'hiking']);
 console.log(person1.getBio());
 
