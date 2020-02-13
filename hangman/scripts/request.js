62f856dcfe3ddc494fd0593ce975ef8180ef4e86
chanmyaeoo <chanmyaeoo1996mgk@gmail.com>
2020-02-13 15:08:45 +0630

    fixed http error

--- hangman/scripts/request.js
+++ hangman/scripts/request.js
@@ -1,6 +1,6 @@
 const getRequest = async wordCount => {
   const response = await fetch(
-    `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`
+    `//puzzle.mead.io/puzzle?wordCount=${wordCount}`
   );
   if (response.status === 200) {
     const data = await response.json();
@@ -30,7 +30,7 @@ const getRequest = async wordCount => {
 // };
 
 const getCountryData = async countryCode => {
-  const response = await fetch('http://restcountries.eu/rest/v2/all');
+  const response = await fetch('//restcountries.eu/rest/v2/all');
   if (response.status === 200) {
     const data = await response.json();
     return data.find(country => country.alpha2Code === countryCode);
@@ -58,7 +58,7 @@ const getCountryData = async countryCode => {
 // });
 
 const getLocation = async () => {
-  const response = await fetch('https://ipinfo.io/json?token=1eae91b68e5661');
+  const response = await fetch('//ipinfo.io/json?token=1eae91b68e5661');
   if (response.status === 200) {
     return response.json();
   } else {
